3b88f4025248d9c134413d7e431864b00f3862e9
RICHARD Sébastien <sebastien-richard3@etud.univ-tln.fr>
2024-04-16 14:14:32 +0200

    Edits main assignment message to show unique values

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 """
 @Author: RICHARD Sébastien
 @Date: 2024-04-15
-@Description: This is the main file for the SAT solver. It will read the input file, and then call the functions in satSolver.py to solve the SAT problem.
+@Description: This is the main file for the SAT solver. It will read the input file, and then call the functions
+in satSolver.py to solve the SAT problem.
 @Input: The input file is a text file with the following format: each line is a clause, and each number is a literal.
-@Output: The output is a text file with the following format: the first line is the number of variables, and the second line is the assignment of the variables.
+@Output: The output is a text file with the following format: the first line is the number of variables,
+and the second line is the assignment of the variables.
 """
 import sys
 
@@ -16,6 +18,6 @@ if __name__ == '__main__':
     if res:
         print("This SAT problem is satisfiable.")
         print("The assignment of the variables is:")
-        print(assignment)
+        print(list(set(assignment)))
     else:
         print("This SAT problem is not satisfiable.")
